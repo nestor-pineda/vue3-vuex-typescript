07ca32bfafd8e664cbf2db7faf03d595bd44cc21
nestor-pineda <nestorpinedaelvira@gmail.com>
2022-12-06 13:41:34 +0100

    fetch action created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
-import { createStore } from "vuex";
-import { State } from "@/interfaces/store.interface";
+import { createStore, createLogger } from "vuex";
+import { State, Post } from "@/interfaces/store.interface";
+
+const plugins = [createLogger()];
 
 // Lectura de estados globales. Variables que crearíamos en el data del options API.
 const state: State = {
   counter: 0,
   selectedNumbers: [],
-  classList: ["nestor", "pedro", "raquel", "marta", "luisa", "jacobo"],
+  classList: ["nestor", "juan", "raquel"],
+  list: [],
+  loading: false,
 };
 // Lectura de estados Computados. Estados que dependen de las variables en state. Tienen valor tangible. Computed Properties that depend on the state
 const getters = {
@@ -32,19 +36,34 @@ const mutations = {
   deleteNumber(state: State, value: number) {
     const itemIndex = state.selectedNumbers.indexOf(value);
     state.selectedNumbers.splice(itemIndex, 1);
-    // state.selectedNumbers = state.selectedNumbers.filter((index) => {
-    //   return value !== index;
-    // });
   },
-
   increaseCounter(state: State, value: number) {
     state.counter = value;
+  },
+  set(state: State, value: Post[]) {
+    state.list = value;
+    console.log(state.list);
+  },
+  setLoading(state: State, loading: boolean) {
+    state.loading = loading;
+  },
+};
+
+// we call async functions and combine them with mutations to set the a global state.
+const actions = {
+  async get({ commit }: any) {
+    const result = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await result.json();
+    commit("set", json);
+    commit("setLoading", false);
   },
 };
 
 // export default createStore({})
 export default createStore({
+  plugins: plugins,
   state: state,
   getters: getters,
   mutations: mutations,
+  actions: actions,
 });
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <h1>This is the About page</h1>
+    <ComponentFive />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ComponentFive from "@/components/ComponentFive.vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "AboutView",
+  components: { ComponentFive },
+
+  setup() {
+    const store = useStore();
+    store.dispatch("get");
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
